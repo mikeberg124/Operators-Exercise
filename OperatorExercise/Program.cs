c2e29bf1a393b337eee2020280bf99d3adf603b2
Michael Berg <mberg12@bu.edu>
2023-04-11 15:19:22 -0400

    added first step

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -8,10 +8,23 @@
             double answer = Circle.AreaOfCircle();
             Console.WriteLine($" The area of your circle is: {answer}");
 
+
+
+
+            var x = 3;
+            var y = 10;
+
+            Console.WriteLine(x += y);
+            Console.WriteLine(x -= y);
+            Console.WriteLine(x *= y);
+            Console.WriteLine(x /= y);
+
             var a = 17;
             var b = 4;
             var quotient = a / b;
             var remainder = a % b;
+
+            if (a == 17 && b == 4)
 
             Console.WriteLine($"{a} / {b} is {quotient} remainder {remainder}");
             Console.WriteLine(a + " / " + b + "is" + quotient + " remainder " + remainder);
