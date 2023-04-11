2e286062c3bb0947152e60c3922fb6cca9665979
Michael Berg <mberg12@bu.edu>
2023-04-11 14:56:08 -0400

    Second Exercise

---
+++ OperatorExercise/Circle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace OperatorExercise
+{
+	public class Circle
+	{
+		public static double AreaOfCircle()
+		{
+
+            Console.WriteLine("Please enter the radius of your circle:");
+            var radius = double.Parse(Console.ReadLine());
+
+            var area = (Math.PI + radius + radius);
+            return area;
+
+        }
+	}
+}
+
--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,20 +4,29 @@
     {
         static void Main(string[] args)
         {
+
+            double answer = Circle.AreaOfCircle();
+            Console.WriteLine($" The area of your circle is: {answer}");
+
             var a = 17;
             var b = 4;
             var quotient = a / b;
             var remainder = a % b;
 
-            Console.WriteLine(a + " / " + b + "is" + quotient + " remainder " + remainder);
             Console.WriteLine($"{a} / {b} is {quotient} remainder {remainder}");
+            Console.WriteLine(a + " / " + b + "is" + quotient + " remainder " + remainder);
 
-
-
-
-
-
+             
         }
 
+        
+
     }
+
+
+
+
 }
+
+
+
